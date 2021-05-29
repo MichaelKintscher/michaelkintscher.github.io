5fe412afaeb6258ec1efd3f528d997ed9cad0e21
Michael Kintscher <michael.troop90@gmail.com>
2021-05-29 13:42:43 -0700

    Added test component, and angular app does not appear to be working anymore.

--- github-pages-hub/src/app/app-routing.module.ts
+++ github-pages-hub/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProfilePicComponent } from './profile-pic/profile-pic.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tester', component: ProfilePicComponent },
+  { path: '', component: AppComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ github-pages-hub/src/app/profile-pic/profile-pic.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-pic',
+  templateUrl: './profile-pic.component.html',
+  styleUrls: ['./profile-pic.component.css']
+})
+
+export class ProfilePicComponent implements OnInit {
+
+  constructor() { }
+
+  // See: https://angular.io/guide/lifecycle-hooks
+  ngOnInit(): void {
+
+  }
+}
