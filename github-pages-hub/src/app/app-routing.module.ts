7e31f2d5392de6910573639f60763ca82a37ec40
Michael Kintscher <michael.troop90@gmail.com>
2021-05-29 13:54:26 -0700

    Revert "Added test component, and angular app does not appear to be working anymore."
    This reverts commit 5fe412afaeb6258ec1efd3f528d997ed9cad0e21.

--- github-pages-hub/src/app/app-routing.module.ts
+++ github-pages-hub/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { ProfilePicComponent } from './profile-pic/profile-pic.component';
 
-const routes: Routes = [
-  { path: 'tester', component: ProfilePicComponent },
-  { path: '', component: AppComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
