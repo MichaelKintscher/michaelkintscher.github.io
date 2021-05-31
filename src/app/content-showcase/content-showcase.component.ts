f98d8122af595b0762ecab8ea4d5a111a9e88137
Michael Kintscher <michael.troop90@gmail.com>
2021-05-31 12:28:37 -0700

    Added Angular databinding
    Added databinding to the content-showcase and showcase-container components, so title and description properties of the showcases can be controlled by the showcase-container.

--- src/app/content-showcase/content-showcase.component.ts
+++ src/app/content-showcase/content-showcase.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content-showcase',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-showcase.component.css']
 })
 export class ContentShowcaseComponent implements OnInit {
+
+  // Properties
+  @Input() title = 'Title';
+  @Input() description = 'A breif description of this showcase!'
 
   constructor() { }
 
--- src/app/showcase-container/showcase-container.component.ts
+++ src/app/showcase-container/showcase-container.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowcaseContainerComponent implements OnInit {
 
+  // Properties
+  showcase1Title = 'Writing';
+  showcase2Title = 'YouTube';
+  showcase3Title = 'Research';
+  showcase4Title = 'Game Dev';
+
+  showcase1Description = 'I write to share my thoughts and experiences and to tell my story.';
+  showcase2Description = 'Videos are my way of teaching through educational and informative content.';
+  showcase3Description = 'My research interests include collaboration software, graphics, and data visualization!';
+  showcase4Description = 'Game development is an opportunity for me to build meaingful experiences!';
+
   constructor() { }
 
   ngOnInit(): void {
