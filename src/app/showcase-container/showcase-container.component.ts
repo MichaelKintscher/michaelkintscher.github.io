47f454b45409d8bb702ee04fc3ebff476aed275f
Michael Kintscher <michael@kintscher.dev>
2022-08-05 14:27:06 -0700

    Pulled Showcase Container data to read from JSON file

--- src/app/showcase-container/showcase-container.component.ts
+++ src/app/showcase-container/showcase-container.component.ts
@@ -9,6 +9,10 @@ import { Subject } from 'rxjs';
 import { YouTubeDataV3 } from '../../types/YouTubeDataV3.interface';
 import { YoutubeUtil } from '../utilities/youtube-util';
 
+import showcaseData from '../../assets/showcase-content/top-showcase.json';
+
+let content = showcaseData.showcaseData as ShowcaseSourceInterface[];
+
 @Component({
   selector: 'app-showcase-container',
   templateUrl: './showcase-container.component.html',
@@ -58,114 +62,8 @@ export class ShowcaseContainerComponent implements OnInit {
   }
 
   populateShowcaseData() {
-    this.showcaseData = [
-      {
-        "title": "Research",
-        "description": "My research interests include collaboration software, graphics, and data visualization!",
-        "fullWidth": true,
-        "cardData": [
-          {
-            "title": "Collaboration Patterns in Augmented Reality",
-            "content": "Aa3",
-            "imageSource": '../../assets/thesis.jpg',
-            "link": "https://repository.asu.edu/items/63098"
-          },
-          {
-            "title": "Real-time AR Video Processing",
-            "content": "Aa3",
-            "imageSource": '../../assets/bridging-realities.png',
-            "link": "https://repository.asu.edu/items/63098"
-          },
-          //{
-          //  "title": "Card C3",
-          //  "content": "Cc3",
-          //  "imageSource": '../../assets/test-image3.jpg',
-          //  "link": ""
-          //}
-        ]
-      },
-      {
-        "title": "Developer",
-        "description": "I develop fullstack applications and useful tools for fun, primarily in .NET and UWP.",
-        "fullWidth": true,
-        "cardData": [
-          //{
-          //  "title": "GitHub",
-          //  "content": ("I love exploring how people interact with computers, and how computers can enable people! Also passionate about visual design & storytelling!").substring(0, 100) + "...",
-          //  "imageSource": 'https://avatars.githubusercontent.com/u/17364071?s=400&u=8fcaf8a5076e6323b71f1b5409b1280a4028a65c&v=4',
-          //  "link": "https://github.com/MichaelKintscher"
-          //},
-          //{
-          //  "title": "Stack Overflow",
-          //  "content": ("I love data, visual storytelling, and studying how computer systems can enhance our ability to work together.").substring(0, 100) + "...",
-          //  "imageSource": 'https://i.stack.imgur.com/bYgz3.jpg?s=420&g=1',
-          //  "link": "https://stackoverflow.com/users/8442938/michael-kintscher-they-them"
-          //},
-          //{
-          //  "title": "LinkedIn",
-          //  "content": ("I've always been an architect and a builder, but I didn't always understand the value of the people in the system.").substring(0, 100) + "...",
-          //  "imageSource": 'https://media-exp1.licdn.com/dms/image/C5603AQHpOp-YVId-XA/profile-displayphoto-shrink_800_800/0/1619819917543?e=1633564800&v=beta&t=yhkeGCCwH1ueeTPC2Cy4C5Ep_AnsbiojNEgcigcYCO0',
-          //  "link": ""
-          //}
-        ]
-      },
-      {
-        "title": "Writing",
-        "description": "I write to share my thoughts and experiences and to tell my story.",
-        "fullWidth": false,
-        "cardData": [
-          {
-            "title": "Medium",
-            "content": ("Entertainer. Storyteller. Thinker. I draw from past experience as an LGBTQ+ person and team leader to explore our world with others through new perspective").substring(0,100) + "...",
-            "imageSource": 'https://miro.medium.com/fit/c/197/197/1*lJBg6kaa3ksF3EnQwDk2qg.png',
-            "link": "https://michael-kintscher.medium.com/"
-          },
-          {
-            "title": "Blog",
-            "content": ("For… sometimes I’ll see an old picture of me?. It’s staring at me…. but there’s an emptiness to its eyes – a hole, a void, something incomplete.").substring(0, 100) + "...",
-            "imageSource": '../../assets/test-image2.jpg',
-            "link": "https://michaelkintscher.website/"
-          },
-          {
-            "title": "LinkedIn",
-            "content": ("I've always been an architect and a builder, but I didn't always understand the value of the people in the system.").substring(0, 100) + "...",
-            "imageSource": 'https://media-exp1.licdn.com/dms/image/C5603AQHpOp-YVId-XA/profile-displayphoto-shrink_800_800/0/1619819917543?e=1633564800&v=beta&t=yhkeGCCwH1ueeTPC2Cy4C5Ep_AnsbiojNEgcigcYCO0',
-            "link": "https://www.linkedin.com/in/michaelkintscher/"
-          }
-        ]
-      },
-      {
-        "title": "YouTube",
-        "description": "Videos are my way of teaching through educational and informative content.",
-        "fullWidth": true,
-        "cardData": this.videoCardData
-      },
-      //{
-      //  "title": "Game Dev",
-      //  "description": "Game development is an opportunity for me to build meaingful experiences!",
-      //  "fullWidth": true,
-      //  "cardData": [
-      //    {
-      //      "title": "Card A4",
-      //      "content": "Aa4",
-      //      "imageSource": '../../assets/test-image1.jpg',
-      //      "link": ""
-      //    },
-      //    {
-      //      "title": "Card B4",
-      //      "content": "Bb4",
-      //      "imageSource": '../../assets/test-image2.jpg',
-      //      "link": ""
-      //    },
-      //    {
-      //      "title": "Card C4",
-      //      "content": "Cc4",
-      //      "imageSource": '../../assets/test-image3.jpg',
-      //      "link": ""
-      //    }
-      //  ]
-      //}
-    ];
+
+    this.showcaseData = content;
   }
 
 }
