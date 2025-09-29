f96118ba4da407cca7a77a56acd6809e05caa6f5
Michael Kintscher <michael@kintscher.dev>
2025-09-28 18:37:02 -0700

    Updated to Angular 18.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 // Material Design
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -27,36 +27,29 @@ import { GallerySectionComponent } from './components/gallery-section/gallery-se
 import { CvPageComponent } from './pages/cv-page/cv-page.component';
 import { SectionedListComponent } from './components/sectioned-list/sectioned-list.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    ResearchProjectComponent,
-    ResearchPageComponent,
-    ResearchAreaComponent,
-    PublicationsPageComponent,
-    PublicationComponent,
-    HomePageComponent,
-    ContactCardComponent,
-    ListSectionComponent,
-    GallerySectionComponent,
-    CvPageComponent,
-    SectionedListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    MatToolbarModule,
-    MatDividerModule,
-    MatSidenavModule,
-    MatCardModule,
-    MatGridListModule,
-    MatTabsModule,
-    MatMenuModule,
-    MatIconModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        ResearchProjectComponent,
+        ResearchPageComponent,
+        ResearchAreaComponent,
+        PublicationsPageComponent,
+        PublicationComponent,
+        HomePageComponent,
+        ContactCardComponent,
+        ListSectionComponent,
+        GallerySectionComponent,
+        CvPageComponent,
+        SectionedListComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatDividerModule,
+        MatSidenavModule,
+        MatCardModule,
+        MatGridListModule,
+        MatTabsModule,
+        MatMenuModule,
+        MatIconModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
