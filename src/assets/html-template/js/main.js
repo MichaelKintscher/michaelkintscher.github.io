68c030774f61b2f2f17d1e3357975a7fd11c0786
Michael Kintscher <michael@kintscher.dev>
2025-12-12 01:09:13 -0700

    Fixed the gallery on the home page.

--- src/assets/html-template/js/main.js
+++ src/assets/html-template/js/main.js
@@ -57,6 +57,25 @@ var settings = {
 			// } else {
 			// 	$header.css("background-size", "auto 140vh");
 			// }
+
+			// Lightbox gallery.
+			$window.on('load', function() {
+
+				$('#two').poptrox({
+					caption: function($a) { return $a.next('h3').text(); },
+					overlayColor: '#2c2c2c',
+					overlayOpacity: 0.85,
+					popupCloserText: '',
+					popupLoaderText: '',
+					selector: '.work-item a.image',
+					usePopupCaption: true,
+					usePopupDefaultStyling: false,
+					usePopupEasyClose: false,
+					usePopupNav: true,
+					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+				});
+
+			});
 		}).observe($body[0]);
 
 })(jQuery);
